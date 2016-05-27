e012fd5a5d3663453d1c629adc6e21e8751b0dc1
최규광 <kisay97@naver.com>
2016-05-27 10:31:07 +0900

    20160527-1030 대기화면

--- app/src/main/java/com/estsoft/splashexample/MainActivity.java
+++ app/src/main/java/com/estsoft/splashexample/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
+
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
     }
 }
